2db40c33a7d62beb66e67483c54efb59192a49a8
thonier <thonier30@gmail.com>
2017-02-24 05:13:11 +0000

    Testing Store

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'store/index'
+  
+  resources :users
+  resources :products
+   root 'store#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
